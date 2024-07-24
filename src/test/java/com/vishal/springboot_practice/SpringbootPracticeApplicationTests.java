d0468cf403b7d00e8bcee734d1df362ea7ca8c14
vsc1985 <vsc1985@gmail.com>
2024-07-24 15:42:05 +0530

    initial project setup

---
+++ src/test/java/com/vishal/springboot_practice/SpringbootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.springboot_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
